86ee67f5f93040472cde41bf21dc2e6e3b4e6dd9
aniketbhaumik <52796425+aniketbhaumik@users.noreply.github.com>
2021-04-07 10:40:23 +0530

    Add files via upload

---
+++ LoginDAL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineLibrary
+{
+    public class LoginDAL
+    {
+
+        public bool VerifyData(Passenger p)
+        {
+            bool loginStatus = false;
+            SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["Airline"].ConnectionString);
+            SqlCommand cmd = new SqlCommand("Select * from Passenger where Email = '" + p.Email + "'and Pwd = '" + p.Pwd + "'", cn);
+            cn.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            if (dr.Read())
+            {
+                cn.Close();
+                loginStatus = true;
+            }
+            else
+            {
+                cn.Close();
+                loginStatus = false;
+            }
+
+            return loginStatus;
+        }
+    }
+}
---
+++ LoginModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AirlineProject.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage ="First name is required")]
+        [Display(Name = "First Name")]
+        [MinLength(1)]
+        public string FName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        [Display(Name = "last Name")]
+        [MinLength(1)]
+        public string LName { get; set; }
+
+        [Required(ErrorMessage ="Email is required")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Enter a password")]
+        [DataType(DataType.Password)]
+        [MinLength(4, ErrorMessage ="password should be of minimum length 4")]
+        [Display(Name = "Password")]
+        public string Pwd { get; set; }
+
+        public string PhoneNo { get; set; }
+
+        public string Addr { get; set; }
+
+        public int PID { get; set; }
+
+        [Required(ErrorMessage ="Age is required")]
+        public int Age { get; set; }
+    }
+}
